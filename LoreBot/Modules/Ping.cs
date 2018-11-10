d0752e0391792387f41605a13ba4a6ef377781a6
Nolan Chancellor <theodreder@gmail.com>
2018-11-10 17:08:33 -0500

    First commit

---
+++ LoreBot/Modules/Ping.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoreBot.Modules
+{
+    [Group("ping")]
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command]
+        public async Task DefaultPingAsync()
+        {
+            await ReplyAsync("Pong!");
+        }
+
+        [Command("user")]
+        public async Task PingUserAsync(SocketGuildUser user)
+        {
+            await ReplyAsync($"Pong! {user.Mention}");
+        }
+    }
+}
---
+++ LoreBot/Program.cs
@@ -0,0 +1,85 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoreBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "NTEwOTE1MjI4NjA3NzA5MjA0.DsjShQ.RSoQfNvhDVqMLuwCg4_YbNV2H4E";
+
+            //event subscription
+            _client.Log += Log;
+            _client.UserJoined += AnnounceUserJoined;
+
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, botToken);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task AnnounceUserJoined(SocketGuildUser user)
+        {
+            var guild = user.Guild;
+            var channel = guild.DefaultChannel;
+            await channel.SendMessageAsync($"You're a nizard, {user.Mention}!");
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot)
+                return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!lore ", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
